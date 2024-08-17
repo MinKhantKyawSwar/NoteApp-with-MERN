3b38693e0b5d6a19d6a5e0027d2711071c86be94
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-08-17 21:01:27 +0630

    validator

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const nav = () => {
   return (
     <nav className="bg-slate-50 py-4 px-10">
-      <h1 className="text-teal-600 font-bold text-4xl">Dnote.io</h1>
+      <h1 className="text-teal-600 font-bold text-4xl">ShareNote.io</h1>
     </nav>
   );
 };
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,8 +1,44 @@
-import React from "react";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import StyledErrorMessage from "./StyledErrorMessage";
+
 const NoteForm = ({ isCreate }) => {
+  const initialValues = {
+    title: "",
+    content: "",
+  };
+
+  const NoteFormSchema = Yup.object({
+    title: Yup.string()
+      .min(3, "Title is too short!")
+      .max(30, "Title is too long!")
+      .required("Title is required."),
+    content: Yup.string()
+      .min(5, "Content is too short.")
+      .required("Content is required."),
+  });
+
+  // const validate = (values, props) => {
+  //   const errors = {};
+
+  //   if (values.title.trim().length < 10) {
+  //     errors.title = "Title must have 10 characters!";
+  //   }
+  //   if (values.content.trim().length < 10) {
+  //     errors.content = "Content must have 10 characters!";
+  //   }
+
+  //   return errors;
+  // };
+
+  const submitHandler = (values) => {
+    console.log(values);
+  };
+
   return (
     <section>
       <div className="flex items-center justify-between">
@@ -13,39 +49,51 @@ const NoteForm = ({ isCreate }) => {
           <ArrowLeftIcon width={22} />
         </Link>
       </div>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="title" className="font-medium block">
-            Note Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
-          />
-        </div>
-        <div className="">
-          <label htmlFor="description" className="font-medium block">
-            Note Description
-          </label>
-          <textarea
-            rows={4}
-            type="text"
-            name="description"
-            id="description"
-            className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
-          />
-        </div>
-        <button className="text-white bg-teal-600 py-4 font-medium w-full text-center">
-          Save
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        // validate={validate}
+        validationSchema={NoteFormSchema}
+        onSubmit={submitHandler}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="title" className="font-medium block">
+                Note Title
+              </label>
+              <Field
+                type="text"
+                name="title"
+                id="title"
+                className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
+              />
+              <StyledErrorMessage name="title" />
+            </div>
+            <div className="">
+              <label htmlFor="content" className="font-medium block">
+                Note Content
+              </label>
+              <Field
+                as="textarea"
+                rows={4}
+                type="text"
+                name="content"
+                id="content"
+                className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
+              />
+              <StyledErrorMessage name="content" />
+            </div>
+            <button
+              className="text-white bg-teal-600 py-4 font-medium w-full text-center"
+              type="submit"
+            >
+              Save
+            </button>
+          </Form>
+        )}
+      </Formik>
     </section>
   );
 };
 
 export default NoteForm;
-
-//42-1
-//20:39
