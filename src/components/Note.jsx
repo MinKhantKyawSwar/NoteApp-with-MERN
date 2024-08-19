44b9791d4566d9277b0201751f8ece90f37b922d
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-08-19 20:48:36 +0630

    completed read

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,26 +5,30 @@ import {
   EyeIcon,
 } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+import formatISO9075 from "date-fns/formatISO9075";
 
-const Note = () => {
+const Note = ({ note }) => {
+  const { _id, title, content, createdAt } = note;
   return (
-    <div className="w-2/5 border border-t-4 border-t-teal-600 mt-10 shadow-lg p-3">
-      <h3 className="text-xl font-medium">Lorem ipsum dolor sit amet</h3>
-      <p className="text-xs">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
-        possimus, culpa perspiciatis
-      </p>
-      <div className="flex items-center justify-end gap-2">
-        <Link to={"/notes/1"}>
-          <EyeIcon width={20} className="text-grey-500 cursor-pointer" />
-        </Link>
-        <Link to={"/edit/1"}>
-          <PencilSquareIcon
-            width={20}
-            className="text-teal-600 cursor-pointer"
-          />
-        </Link>
-        <TrashIcon width={20} className="text-red-600 cursor-pointer" />
+    <div className="w-2/5 border border-t-4 border-t-teal-600 mt-10 shadow-lg p-3 h-fit">
+      <h3 className="text-xl font-medium">{title}</h3>
+      <p className="text-xs">{content.slice(0, 150)}</p>
+      <div className="flex items-center justify-between mt-2 border-t pt-2">
+        <p className="text-s font-medium">
+          {formatISO9075(new Date(createdAt), { representation: "date" })}
+        </p>
+        <div className="flex items-center justify-end gap-2">
+          <Link to={"/notes/" + _id}>
+            <EyeIcon width={20} className="text-grey-500 cursor-pointer" />
+          </Link>
+          <Link to={"/edit/" + _id}>
+            <PencilSquareIcon
+              width={20}
+              className="text-teal-600 cursor-pointer"
+            />
+          </Link>
+          <TrashIcon width={20} className="text-red-600 cursor-pointer" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,25 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import { Link, useParams } from "react-router-dom";
+import { UserIcon, CalendarDaysIcon } from "@heroicons/react/24/solid";
+import formatISO9075 from "date-fns/formatISO9075";
 
 const Details = () => {
+  const { id } = useParams();
+
+  const [note, setNote] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const getNote = async () => {
+    setLoading(true);
+    const response = await fetch(`${import.meta.env.VITE_API}/notes/${id}`);
+    const note = await response.json();
+    setNote(note);
+    setLoading(false);
+  };
+
+  useEffect((_) => {
+    getNote();
+  }, []);
+
   return (
-    <section className="px-10 mt-10">
-      <div className="text-right">
-        <Link
-          to={"/"}
-          className="\ text-teal-600 font-medium border border-teal-600 px-3 py-2"
-        >
-          Back
-        </Link>
-      </div>
-      <div className="border border-t-4 border-t-teal-600 shadow-lg p-3 mt-4">
-        <h3 className="text-3xl font-medium">Lorem ipsum dolor sit amet</h3>
-        <p className="text-base mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rem
-          possimus, culpa perspiciatis
-        </p>
-      </div>
-    </section>
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center align-center w-full">
+          <TailSpin
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <section className="px-10 mt-10">
+          <div className="text-right">
+            <Link
+              to={"/"}
+              className="\ text-teal-600 font-medium border border-teal-600 px-3 py-2"
+            >
+              Back
+            </Link>
+          </div>
+          <div className="border border-t-4 border-t-teal-600 shadow-lg p-3 mt-4">
+            <h3 className="text-3xl font-medium">{note.title}</h3>
+            <div>
+              <p className="flex items-center gap-1 font-sm text-gray-600">
+                <UserIcon className="w-4 h-4" />
+                {note.author}
+              </p>
+              {note.createdAt ? (
+                <p className="flex items-center gap-1 font-sm text-gray-600">
+                  <CalendarDaysIcon className="w-4 h-4" />
+                  {formatISO9075(new Date(), {
+                    representation: "date",
+                  })}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+            <p className="text-base mt-2">{note.content}</p>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
