d828e02244a8d486ecd16800d9313052f69b82dd
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-08-23 22:54:59 +0630

    register

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,10 +1,16 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
+import { Slide, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 //formik custom error message
 import StyledErrorMessage from "./StyledErrorMessage";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const AuthForm = ({ isLogin }) => {
+  const [redirect, setReirect] = useState(false);
   const initialValues = {
     username: "",
     email: "",
@@ -24,12 +30,58 @@ const AuthForm = ({ isLogin }) => {
       .required("Password is required."),
   });
 
-  const submitHandler = (values) => {
-    console.log(values);
+  const submitHandler = async (values) => {
+    const { email, password, username } = values;
+    if (isLogin) {
+      //login codes
+    } else {
+      const response = await fetch(`${import.meta.env.VITE_API}/register`, {
+        method: "POST",
+        body: JSON.stringify({ email, password, username }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+      if (response.status === 201) {
+        setReirect(true);
+      } else if (response.status === 400) {
+        const { errorMessages } = await response.json();
+        const pickedMessage = errorMessages[0].msg;
+        toast.error(pickedMessage, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Slide,
+        });
+      }
+    }
   };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
       <Formik
         initialValues={initialValues}
         validationSchema={AuthFormSchema}
@@ -40,19 +92,20 @@ const AuthForm = ({ isLogin }) => {
             <h1 className="text-center font-semibold text-4xl my-4 text-teal-600">
               {isLogin ? "Login" : "Register"}
             </h1>
-            <div className="mb-3">
-              <label htmlFor="username" className="font-medium block">
-                username
-              </label>
-              <Field
-                type="text"
-                name="username"
-                id="username"
-                className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
-              />
-              <StyledErrorMessage name="username" />
-            </div>
-
+            {!isLogin && (
+              <div className="mb-3">
+                <label htmlFor="username" className="font-medium block">
+                  username
+                </label>
+                <Field
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="text-lg border-2 border-teal-600 py-1 w-full rounded-lg"
+                />
+                <StyledErrorMessage name="username" />
+              </div>
+            )}
             <div className="mb-3">
               <label htmlFor="email" className="font-medium block">
                 email
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,21 @@ import { Link } from "react-router-dom";
 const nav = () => {
   return (
     <nav className="bg-slate-50 py-4 px-10 flex items-center justify-between font-mono">
-      <h1 className="text-teal-600 font-bold text-4xl">ShareNote.io</h1>
-      <div>
+      <Link to={"/"} className="text-teal-600 font-bold text-4xl">
+        ShareNote.io
+      </Link>
+      <div className="flex gap-3">
         <Link to={"/create"} className="text-teal-600 font-medium">
           {" "}
           SHARE{" "}
+        </Link>
+        <Link to={"/login"} className="text-teal-600 font-medium">
+          {" "}
+          LOGIN{" "}
+        </Link>
+        <Link to={"/register"} className="text-teal-600 font-medium">
+          {" "}
+          REGISTER{" "}
         </Link>
       </div>
     </nav>
